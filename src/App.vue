31ab951a3592cd63354cfbb96b4bc8303503fae7
PrzemyslawRomanczyk <49125731+PrzemyslawRomanczyk@users.noreply.github.com>
2020-08-26 19:27:10 +0200

    Adding banner carousel

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <Baner></Baner>
     <Form></Form>
     <ListOfUsers></ListOfUsers>
-    <Baner></Baner>
   </div>
 </template>
 
--- src/components/Baner.vue
+++ src/components/Baner.vue
@@ -1,16 +1,49 @@
 <template>
     <div>
-        Baner
+        <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="3000"
+            controls
+            background="#ababab"
+            img-width="1024"
+            img-height="480"
+            style="text-shadow: 1px 1px 2px #333;"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+        >
+            <b-carousel-slide img-src="../assets/bannerPic1.jpg"></b-carousel-slide>
+            <b-carousel-slide img-src="../assets/bannerPic2.jpg"></b-carousel-slide>
+            <b-carousel-slide img-src="../assets/bannerPic3.jpg"></b-carousel-slide>
+        </b-carousel>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    name: 'Baner'
+    name: 'Baner',
+    data() {
+      return {
+        slide: 0,
+        sliding: true
+    }
+    },
+    methods: {
+      onSlideStart(slide: any) {
+        this.sliding = true
+      },
+      onSlideEnd(slide: any) {
+        this.sliding = false
+      }
+    }
 })
 </script>
 
-<style lang="sass">
+<style scoped>
+    div {
+        width: 100%;
 
+    }
 </style>
+
